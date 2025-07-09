5b6310468155436bef66786c58503802f0294aef
Jan Ali Hassan <68240754+janali-17@users.noreply.github.com>
2025-07-09 11:45:10 +0500

    feat: Door Animation(Door_V1) and Player interaction added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,51 @@
+using StarterAssets;
 using UnityEngine;
-using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
-    public PlayerInfo Info;
+    [SerializeField] private Camera playerCamera;
 
-    void Start()
+
+    private float interactDistance = 3f;
+    private StarterAssetsInputs inputSystem;
+    private bool isDoorOpen;
+
+    private void Start()
     {
-        // Example initialization
-        Info = new PlayerInfo(PlayerType.Human, transform.position, transform.forward, new List<string>()); // Default right now, will change later
+        inputSystem = GetComponent<StarterAssetsInputs>();
+
+        inputSystem.OnInteractPlayer += InputSystem_OnInteractPlayer;
     }
 
-    void Update()
+    private void Update()
     {
-        // Update the info with the current position and direction
-        Info.Position = transform.position;
-        Info.Direction = transform.forward;
+       // CheckForGameObjectInView();
     }
 
-    // Example: Add a powerup
-    public void AddPowerup(string powerup)
+    private void CheckForGameObjectInView()
     {
-        if (!Info.CurrentPowerups.Contains(powerup))
-            Info.CurrentPowerups.Add(powerup);
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width /2 , Screen.height /2));
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * interactDistance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if(hit.collider.CompareTag("Door"))
+            {
+                DoorAnimtion doorAnim = hit.collider.GetComponent<DoorAnimtion>();
+                if (doorAnim != null)
+                {
+                   doorAnim.ToggleDoor();
+                }
+            }
+        }
     }
+
+
+    private void InputSystem_OnInteractPlayer(object sender, System.EventArgs e)
+    {
+        CheckForGameObjectInView();
+    }
+
 }
