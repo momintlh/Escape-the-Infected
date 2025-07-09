7766127837b1a1814780b870c025b5bd444198f5
Jan Ali Hassan <68240754+janali-17@users.noreply.github.com>
2025-07-09 11:45:10 +0500

    feat: Door Animation(Door_V1) and Player interaction added

--- Assets/Scripts/Door/DoorAnimtion.cs
+++ Assets/Scripts/Door/DoorAnimtion.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class DoorAnimtion : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private Animator animator;
+    private bool isOpen = false;
+
+    private void Awake()
     {
-        
+        animator = GetComponentInParent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleDoor()
     {
-        
+        if (isOpen)
+        {
+            DoorCloseAnim();
+        }
+        else
+        {
+            DoorOpenAnim();
+        }
+    }
+
+    public void DoorOpenAnim()
+    {
+        if (isOpen) return;
+        animator.SetTrigger("DoorOpen");
+        isOpen = true;
+    }
+
+    public void DoorCloseAnim()
+    {
+        if(!isOpen) return;
+        animator.SetTrigger("DoorClose");
+        isOpen = false;
     }
 }
